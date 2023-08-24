79da456d43e693d87ec3f8724e78cba889141a21
port <portdev@protonmail.com>
2023-08-24 11:09:19 +0300

    Set up react

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+      porro, veritatis sit ut esse vero enim dignissimos, laborum a cupiditate
+      reiciendis. Esse, ipsum et officiis asperiores molestias vel delectus
+      consectetur?
+    </div>
+  );
+}
--- src/renderer.ts
+++ src/renderer.ts
@@ -26,6 +26,11 @@
  * ```
  */
 
-import './index.css';
+import "./index.css";
 
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
+console.log(
+  '👋 This message is being logged by "renderer.js", included via webpack'
+);
+
+// Add this to the end of the existing file
+import "./index.tsx";
